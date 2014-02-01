c91512eaa0a75491317e9f81ff0712852ba5795d
Garth Gaughan <garth0323@gmail.com>
2014-02-01 15:54:39 -0700

    1:m association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140131181640) do
+ActiveRecord::Schema.define(version: 20140201224244) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20140131181640) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id"
   end
+
+  add_index "users", ["post_id"], name: "index_users_on_post_id"
 
 end
